be03ffbc533c239a8e357e7e459fec7cf82c57f6
OnlyDragon0403 <mokhnoalina@gmail.com>
2022-10-17 15:51:29 +0900

    "add:test_with_dll&test_on_unreal_plugin&&update:curl_opt"

---
+++ src/main.h
@@ -0,0 +1,16 @@
+#ifndef VERTICES_SDK_MAIN_H
+#define VERTICES_SDK_MAIN_H
+
+
+#if defined _WIN32 || defined _WIN64
+#define EXAMPLELIBRARY_IMPORT __declspec(dllimport)
+#elif defined __linux__
+#define EXAMPLELIBRARY_IMPORT __attribute__((visibility("default")))
+#else
+#define EXAMPLELIBRARY_IMPORT
+#endif
+
+EXAMPLELIBRARY_IMPORT void ExampleLibraryFunction();
+
+
+#endif //VERTICES_SDK_MAIN_H
